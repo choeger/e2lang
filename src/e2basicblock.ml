6370e62d932703a409189d290e178562a243d8a2
buzatrice <beatrice.tirziu@campus.tu-berlin.de>
2014-05-20 13:00:04 +0200

    complete basic block generation function

--- src/e2basicblock.ml
+++ src/e2basicblock.ml
@@ -22,7 +22,19 @@ let rec build_blocks name stmts = function
             | Label str ->
                     let lblname = "$lbl$" ^ str in
                         let nb = build_blocks lblname stmts (ei+1, ei+1) in
-                        {name = name; start_pos = si+1; end_pos = ei+1; next = OneBlock lblname} :: nb
+                        {name = name; start_pos = si; end_pos = ei; next = OneBlock lblname} :: nb
+            | Jmp str ->
+                    let new_name = "after Jump" ^ name in
+                        let nb = build_blocks new_name stmts (ei+1, ei+1) in
+                        {name = name; start_pos = si; end_pos = ei; next = OneBlock str}::nb
+            | CJmp (str, cond) -> 
+                    let new_name = "after CJump" ^ name in
+                        let nb = build_blocks new_name stmts (ei+1, ei+1) in
+                        {name = name; start_pos = si; end_pos = ei+1; next = OneBlock str}::nb
+            | Ret arg -> 
+                    let new_name = "after returning" ^ name in
+                        let nb = build_blocks new_name stmts (ei+1, ei+1) in
+                        {name = name; start_pos = si; end_pos = ei+1; next = NoBlock}::nb
             | _ -> build_blocks name stmts (si, ei + 1)
 
 let build stmts = build_blocks "start" stmts (0, 0)
