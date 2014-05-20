6196c511003dd2040cd31182e7a85be6018683b6
Florian Sachse <sachse.florian@googlemail.com>
2014-05-20 12:36:10 +0200

    fixed the label after label issue (introduced empty blocks)

--- src/e2basicblock.ml
+++ src/e2basicblock.ml
@@ -16,17 +16,14 @@ let rec build_blocks name stmts = function
     (si, ei) ->
         if ei > Array.length(stmts)
         then
-            [{name = name; start_pos = si; end_pos = (ei-1); next = NoBlock}]
+            [{name = name; start_pos = si; end_pos = ei; next = NoBlock}]
         else
             match stmts.(ei) with
             | Label str ->
                     let lblname = "$lbl$" ^ str in
-                    if ei > si
-                    then
                         let nb = build_blocks lblname stmts (ei+1, ei+1) in
-                        {name = name; start_pos = si; end_pos = (ei-1); next = OneBlock lblname} :: nb
-                    else
-                        build_blocks lblname stmts (si+1, ei+1)
+                        {name = name; start_pos = si+1; end_pos = ei+1; next = OneBlock lblname} :: nb
             | _ -> build_blocks name stmts (si, ei + 1)
 
 let build stmts = build_blocks "start" stmts (0, 0)
+
