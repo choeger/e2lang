cddc0aa628a3b8886504d21ce0b6d64af144f957
Florian Sachse <florian.sachse@campus.tu-berlin.de>
2014-06-25 12:16:06 +0200

    ...

---
+++ src/e2optimize.ml
@@ -0,0 +1,14 @@
+
+open E2lang
+open E2basicblock
+open E2liveness
+
+let optimize (Proc(proto, stmts)) =
+    let bbs = build stmts in
+    let lbs = build_lbs bbs in
+    let g = build_graph proto.dvars lbs in
+    let (coloring, cmax) = color_graph g in
+    print_graph g;
+    let colored_bbs = apply_coloring coloring bbs in
+    let colored_proto = {proto with dvars=cmax+1} in
+    (colored_proto, colored_bbs)
